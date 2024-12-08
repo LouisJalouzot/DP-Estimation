674e519f7aec26fafdf52eb7e7bdcb8b4ccbd619
Louis Jalouzot <louis.jalouzot@ens-lyon.fr>
2024-12-08 21:50:28 +0100

    Format

--- RDPexperiments_theory.py
+++ RDPexperiments_theory.py
@@ -1,16 +1,17 @@
-from estimatorRDP import Estimator
-from RDPtruncatedLaplace import TruncatedLaplace
 import matplotlib.pyplot as plt
 import numpy as np
+
+from estimatorRDP import Estimator
+from RDPtruncatedLaplace import TruncatedLaplace
+
 
 def experiments(gamma, delta, B, runs, mu1, mu2):
     mechanism = TruncatedLaplace(mu=mu1, B=B)
     mechanism_ = TruncatedLaplace(mu=mu2, B=B)
     epsilon = mechanism.epsilon
-    print('epsilon =', epsilon, 'C =', mechanism.C)
-    est = Estimator(gamma=gamma, delta=delta, epsilon=epsilon,
-                    C=mechanism.C)
-    print(f'm = {est.m}, n = {est.n}')
+    print("epsilon =", epsilon, "C =", mechanism.C)
+    est = Estimator(gamma=gamma, delta=delta, epsilon=epsilon, C=mechanism.C)
+    print(f"m = {est.m}, n = {est.n}")
     indices = [est.n]
     while indices[-1] > 10:
         indices.append(indices[-1] // 8)
@@ -21,21 +22,24 @@ def experiments(gamma, delta, B, runs, mu1, mu2):
         # print('i =', i)
         mean, std, prop = est.run_estimator(mechanism, mechanism_, i, runs)
         if prop >= delta and practical_n is None:
-            a, b = i//2, i
+            a, b = i // 2, i
             while b - a > 2:
-                k = (b + a)//2
-                mean, std, prop = est.run_estimator(mechanism, mechanism_,
-                                                    k, runs)
+                k = (b + a) // 2
+                mean, std, prop = est.run_estimator(
+                    mechanism, mechanism_, k, runs
+                )
                 if prop >= delta:
                     b = k
-                else: a = k
+                else:
+                    a = k
             while prop < delta:
-                mean, std, prop = est.run_estimator(mechanism, mechanism_,
-                                                    b, runs)
+                mean, std, prop = est.run_estimator(
+                    mechanism, mechanism_, b, runs
+                )
             i = b
             practical_n = b
             practical_n_index = len(nb)
-            print('practical', b)
+            print("practical", b)
         if mean is not None:
             nb.append(i)
             means.append(mean)
@@ -45,49 +49,66 @@ def experiments(gamma, delta, B, runs, mu1, mu2):
     nb = np.array(nb)
     means = np.array(means)
     stds = np.array(stds)
-    plt.rc('axes', titlesize=16, labelsize=12,
-           titleweight='bold', labelweight='bold')
-    plt.rc('figure', titlesize=18, titleweight='bold')
+    plt.rc(
+        "axes",
+        titlesize=16,
+        labelsize=12,
+        titleweight="bold",
+        labelweight="bold",
+    )
+    plt.rc("figure", titlesize=18, titleweight="bold")
     fig = plt.figure(figsize=(20, 10))
     ax = fig.add_subplot(211)
-    ax.axhline(y=epsilon, color='red',
-               label=r"real $\epsilon$", linestyle='--')
-    ax.axhspan(epsilon - gamma, epsilon + gamma, facecolor='orange', alpha=.2,
-               label=r'$\gamma$-width band around $\epsilon$ '
-               + '(required output)')
+    ax.axhline(y=epsilon, color="red", label=r"real $\epsilon$", linestyle="--")
+    ax.axhspan(
+        epsilon - gamma,
+        epsilon + gamma,
+        facecolor="orange",
+        alpha=0.2,
+        label=r"$\gamma$-width band around $\epsilon$ " + "(required output)",
+    )
     # ax.axhline(y=epsilon + gamma, color='red', linestyle=':',
-                # label=r'$\epsilon + \gamma$ and $\epsilon - \gamma$')
+    # label=r'$\epsilon + \gamma$ and $\epsilon - \gamma$')
     # ax.axhline(y=epsilon - gamma, color='red', linestyle=':')
-    ax.fill_between(nb, means - stds, means + stds, alpha=.2,
-                    label=r'standard deviation of $\tilde{\epsilon}$ '
-                    + f'across {runs} executions')
+    ax.fill_between(
+        nb,
+        means - stds,
+        means + stds,
+        alpha=0.2,
+        label=r"standard deviation of $\tilde{\epsilon}$ "
+        + f"across {runs} executions",
+    )
     # ax.errorbar(nb, means, stds, linestyle='None',
-                # marker='o', capsize=3,
-                # label='Confidence intervals of level '
-                # + rf'$\delta=${delta} across {runs} executions')
-    ax.plot(nb, means, label=r'mean of $\tilde{\epsilon}$')
-    ax.set_title(r'Estimated $\mathbf{\tilde{\epsilon}}$ '
-                 + 'against the number of samples')
+    # marker='o', capsize=3,
+    # label='Confidence intervals of level '
+    # + rf'$\delta=${delta} across {runs} executions')
+    ax.plot(nb, means, label=r"mean of $\tilde{\epsilon}$")
+    ax.set_title(
+        r"Estimated $\mathbf{\tilde{\epsilon}}$ "
+        + "against the number of samples"
+    )
     # ax.set_ylabel(r'Estimated $\mathbf{\tilde{\epsilon}}$')
-    ax.set_xlabel(r'Number of samples $\mathbf{n}$ (log scale)')
-    ax.set_xscale('log')
+    ax.set_xlabel(r"Number of samples $\mathbf{n}$ (log scale)")
+    ax.set_xscale("log")
     # ax.set_ylim(top=np.max(means))
     ax.legend()
     ax2 = fig.add_subplot(212)
-    ax2.axhline(y=delta, color='purple', label=r'$\delta$')
-    bar = ax2.bar([str(i) for i in nb], props, color='purple', alpha=.4)
-    labels = [''] * len(props)
-    labels[practical_n_index] = 'Practical n'
-    labels[-1] = 'Theoretical n'
-    ax2.bar_label(bar, labels, label_type='center')
+    ax2.axhline(y=delta, color="purple", label=r"$\delta$")
+    bar = ax2.bar([str(i) for i in nb], props, color="purple", alpha=0.4)
+    labels = [""] * len(props)
+    labels[practical_n_index] = "Practical n"
+    labels[-1] = "Theoretical n"
+    ax2.bar_label(bar, labels, label_type="center")
     ax2.legend()
-    ax2.set_xlabel(r'Number of samples $\mathbf{n}$')
-    ax2.set_ylabel(f'Proportion across {runs} executions')
-    ax2.set_title(r'Proportion of estimated $\mathbf{\tilde{\epsilon}}$ '
-                   + r'close to $\mathbf{\epsilon}$ within $\mathbf{\gamma}$')
-    fig.tight_layout(pad=5) 
+    ax2.set_xlabel(r"Number of samples $\mathbf{n}$")
+    ax2.set_ylabel(f"Proportion across {runs} executions")
+    ax2.set_title(
+        r"Proportion of estimated $\mathbf{\tilde{\epsilon}}$ "
+        + r"close to $\mathbf{\epsilon}$ within $\mathbf{\gamma}$"
+    )
+    fig.tight_layout(pad=5)
     plt.show(block=False)
-    
+
     X, Y = mechanism.true_density()
     _, Y_ = mechanism_.true_density()
     hist = {}
@@ -139,11 +160,11 @@ def experiments(gamma, delta, B, runs, mu1, mu2):
     # ax2.set_xlabel('z')
     # ax2.legend()
     plt.show()
-    
+
 
 if __name__ == "__main__":
-    gamma = .5
-    delta = .9
+    gamma = 0.5
+    delta = 0.9
     runs = 100
     mu1 = 0
     mu2 = 1
--- table practical RDP.py
+++ table practical RDP.py
@@ -1,18 +1,20 @@
-from estimatorRDP import Estimator
-from RDPtruncatedLaplace import TruncatedLaplace
 import matplotlib.pyplot as plt
 import numpy as np
 
+from estimatorRDP import Estimator
+from RDPtruncatedLaplace import TruncatedLaplace
+
+
 def experiments(runs):
-    delta = .9
+    delta = 0.9
     for B in [5, 3, 2, 1.5]:
         mechanism = TruncatedLaplace(mu=0, B=B)
         mechanism_ = TruncatedLaplace(mu=1, B=B)
         epsilon = mechanism.epsilon
-        print("eps:", int(1000*epsilon)/1000)
+        print("eps:", int(1000 * epsilon) / 1000)
         print("C:", int(100 * mechanism.C) / 100)
         # for gamma in [10, 1, .5, .1, .01]:
-        for gamma in [1, .5, .1]:
+        for gamma in [1, 0.5, 0.1]:
             print("    gamma:", gamma)
             est = Estimator(gamma=gamma, epsilon=epsilon, C=mechanism.C)
             n = 100
@@ -23,19 +25,21 @@ def experiments(runs):
             if n > 1e9 and prop < delta:
                 n = None
             else:
-                a, b = n//4, n//2
+                a, b = n // 4, n // 2
                 while b - a > 10:
-                    k = (b + a)//2
-                    _, _, prop = est.run_estimator(mechanism, mechanism_, k, runs)
+                    k = (b + a) // 2
+                    _, _, prop = est.run_estimator(
+                        mechanism, mechanism_, k, runs
+                    )
                     if prop >= delta:
                         b = k
-                    else: a = k
+                    else:
+                        a = k
                 n = b
             print("        m:", est.m)
             print("        Theoretical n:", est.n)
             print("        Practical n:", n)
-            
-    
+
 
 if __name__ == "__main__":
     runs = 1000
