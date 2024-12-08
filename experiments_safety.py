1d67a60aa8c577fdd86f4da2d925a660b3835be5
Louis Jalouzot <86967472+LouisJalouzot@users.noreply.github.com>
2024-12-08 21:50:28 +0100

    Format

--- experiments_safety.py
+++ experiments_safety.py
@@ -1,9 +1,12 @@
-from estimator import Estimator
-from truncatedLaplace import TruncatedLaplace
 import matplotlib.pyplot as plt
 import numpy as np
-gammaR = r'$\mathbf{\gamma}$'
-deltaR = r'$\mathbf{\delta}$'
+
+from estimator import Estimator
+from truncatedLaplace import TruncatedLaplace
+
+gammaR = r"$\mathbf{\gamma}$"
+deltaR = r"$\mathbf{\delta}$"
+
 
 def experiments(gamma, delta, epsilon, runs, mu1, mu2, C, safety_conf):
     bound, props, actualC = [], [], []
@@ -11,9 +14,13 @@ def experiments(gamma, delta, epsilon, runs, mu1, mu2, C, safety_conf):
     while prop > safety_conf or len(bound) < 10:
         mechanism = TruncatedLaplace(mu=mu1, epsilon=epsilon)
         mechanism_ = TruncatedLaplace(mu=mu2, epsilon=epsilon)
-        est = Estimator(gamma=gamma, delta=delta,
-                        C=C,
-                        enable_safety=True, safety_conf=safety_conf)
+        est = Estimator(
+            gamma=gamma,
+            delta=delta,
+            C=C,
+            enable_safety=True,
+            safety_conf=safety_conf,
+        )
         print(f"m = {est.m}, n = {est.n}")
         print(f"Claimed C = {C}, actual C = {mechanism.C}")
         actualC.append(mechanism.C)
@@ -23,35 +30,48 @@ def experiments(gamma, delta, epsilon, runs, mu1, mu2, C, safety_conf):
         prop = est.run_estimator(mechanism, mechanism_, est.n, runs)
         print(prop)
         props.append(prop)
-        print('\n')
-        epsilon += .15
+        print("\n")
+        epsilon += 0.15
     plt.figure(figsize=(10, 6))
-    plt.rc('axes', titlesize=16, labelsize=12,
-           titleweight='bold', labelweight='bold')
-    plt.rc('figure', titlesize=18, titleweight='bold')
+    plt.rc(
+        "axes",
+        titlesize=16,
+        labelsize=12,
+        titleweight="bold",
+        labelweight="bold",
+    )
+    plt.rc("figure", titlesize=18, titleweight="bold")
     plt.plot(actualC, props, linewidth=3)
-    plt.axhline(safety_conf,
-                label='required safety check confidence '
-                + f'({int(safety_conf*100)}%)',
-                color='orange', linewidth=3)
-    plt.xlabel(f'Actual lipschitz constant C for a claimed C = {C}')
-    plt.ylabel(f'Proportion across {runs} executions')
-    plt.title('Proportion of executions which '
-              + '''did not trigger the safety check
-              '''
-              + f'(m = {est.m}, n = {est.n}, '
-              + gammaR + f' = {gamma}, ' + deltaR + f' = {delta})')
+    plt.axhline(
+        safety_conf,
+        label="required safety check confidence "
+        + f"({int(safety_conf*100)}%)",
+        color="orange",
+        linewidth=3,
+    )
+    plt.xlabel(f"Actual lipschitz constant C for a claimed C = {C}")
+    plt.ylabel(f"Proportion across {runs} executions")
+    plt.title(
+        "Proportion of executions which "
+        + """did not trigger the safety check
+              """
+        + f"(m = {est.m}, n = {est.n}, "
+        + gammaR
+        + f" = {gamma}, "
+        + deltaR
+        + f" = {delta})"
+    )
     plt.legend()
     plt.show()
-    
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     gamma = 2
-    delta = .8
-    epsilon = .5
+    delta = 0.8
+    epsilon = 0.5
     mu1 = 0
     mu2 = 1
     C = 1.7
     runs = 100
-    safety_conf = .5
+    safety_conf = 0.5
     experiments(gamma, delta, epsilon, runs, mu1, mu2, C, safety_conf)
--- table practical.py
+++ table practical.py
@@ -1,28 +1,36 @@
 from estimator import Estimator
 from truncatedLaplace import TruncatedLaplace
-import matplotlib.pyplot as plt
-import numpy as np
+
 
 def experiments(runs):
-    delta = .8
+    delta = 0.8
     # for epsilon in [.5, 1, 3, 5, 10]:
-    for epsilon in [.5, .7, 1, 2]:
+    for epsilon in [0.5, 0.7, 1, 2]:
         mechanism = TruncatedLaplace(mu=0, epsilon=epsilon)
         mechanism_ = TruncatedLaplace(mu=1, epsilon=epsilon)
         epsilon = mechanism.epsilon
         print("eps:", epsilon)
         print("C:", int(100 * mechanism.C) / 100)
         # for gamma in [10, 1, .5, .1, .01]:
-        for gamma in [1, .5, .1, .05]:
+        for gamma in [1, 0.5, 0.1, 0.05]:
             print("    gamma:", gamma)
             try:
-                est = Estimator(gamma=gamma, delta=delta, epsilon=epsilon,
-                                C=mechanism.C,
-                                enable_safety=False)
+                est = Estimator(
+                    gamma=gamma,
+                    delta=delta,
+                    epsilon=epsilon,
+                    C=mechanism.C,
+                    enable_safety=False,
+                )
             except:
-                est = Estimator(gamma=gamma, delta=delta, epsilon=epsilon,
-                                C=mechanism.C, m=int(100/gamma),
-                                enable_safety=False)
+                est = Estimator(
+                    gamma=gamma,
+                    delta=delta,
+                    epsilon=epsilon,
+                    C=mechanism.C,
+                    m=int(100 / gamma),
+                    enable_safety=False,
+                )
                 est.n = None
             m = est.m
             n = 100
@@ -33,20 +41,19 @@ def experiments(runs):
             if n > 1e9 and prop < delta:
                 n = None
             else:
-                a, b = n//4, n//2
+                a, b = n // 4, n // 2
                 while b - a > m / gamma:
-                    k = (b + a)//2
-                    prop = est.run_estimator2(mechanism, mechanism_,
-                                                    k, runs)
+                    k = (b + a) // 2
+                    prop = est.run_estimator2(mechanism, mechanism_, k, runs)
                     if prop >= delta:
                         b = k
-                    else: a = k
+                    else:
+                        a = k
                 n = b
             print("        m:", est.m)
             print("        Theoretical n:", est.n)
             print("        Practical n:", n)
-            
-    
+
 
 if __name__ == "__main__":
     runs = 100
